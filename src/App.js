65b39222063483cad466632deaf67dc2b46c7dff
iamfinnz <ilham19ti@mahasiswa.pcr.ac.id>
2024-04-16 19:37:46 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import { getMovie, searchMovie } from './api';
 
-function App() {
+const App = () => {
+
+  const [popularMovies, setPopularMovies] = useState([])
+
+  useEffect(() => {
+    getMovie().then((result) => {
+      setPopularMovies(result)
+    })
+  }, [])
+
+  // console.log({popularMovies: popularMovies})
+
+  const ShowPopularMovie = () => {
+    return popularMovies.map((movie, i) => {
+      return(
+        <div className="Movie-wrapper" key={i}>
+          <div className="Movie-title">{movie.title}</div>
+          <img className="Movie-image" src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`} />
+          <div className="Movie-date">release: {movie.release_date}</div>
+          <div className="Movie-rate">{movie.vote_average}</div>
+        </div>
+      )
+    })
+  }
+
+  const search = async (q) => {
+    if (q.length > 3) {
+      const cari = await searchMovie(q)
+      // console.log({cari: cari})
+      setPopularMovies(cari.results)
+    }
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Ilham Movie</h1>
+        <input
+        className='Movie-search'
+        placeholder='Cari film kesayangan kamu'
+        onChange={({target}) => search(target.value)} />
+        <div className="Movie-container">
+          <ShowPopularMovie />
+        </div>
       </header>
     </div>
   );
---
+++ src/api.js
@@ -0,0 +1,14 @@
+import axios from "axios"
+
+const baseUrl =  process.env.REACT_APP_BASEURL
+const apiKey = process.env.REACT_APP_APIKEY
+
+export const getMovie = async() => {
+    const movie = await axios.get(`${baseUrl}/movie/popular?page=1&api_key=${apiKey}`)
+    return movie.data.results
+}
+
+export const searchMovie = async(q) => {
+    const search = await axios.get(`${baseUrl}/search/movie?query=${q}&page=1&api_key=${apiKey}`)
+    return search.data
+}
